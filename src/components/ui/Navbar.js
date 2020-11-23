a9caad5e732ac1580ff105c9e3e378a88df67b7c
Luis Garcia <lalejandro.1988@gmail.com>
2020-11-22 22:40:23 -0600

    DC and Marvel screens and routes

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                SuperHeroes
+            </Link>
+
+            <button 
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+
+                    <NavLink 
+                        activeClassName="active"
+                        className="nav-item nav-link" 
+                        exact
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+
+                    <NavLink 
+                        activeClassName="active"
+                        className="nav-item nav-link" 
+                        exact
+                        to="/Marvel"
+                    >
+                        Marvel
+                    </NavLink>
+                </ul>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul className="navbar-nav ml-auto">
+                    <NavLink 
+                        activeClassName="active"
+                        className="nav-item nav-link" 
+                        exact
+                        to="/login"
+                    >
+                        Logout
+                    </NavLink>
+                </ul>
+            </div>
+        </nav>
+    )
+}
