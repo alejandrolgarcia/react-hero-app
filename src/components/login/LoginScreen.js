4c7f62d50094b2e8ddb2863e3e911a81b0c2680a
Luis Garcia <lalejandro.1988@gmail.com>
2020-11-26 19:44:14 -0600

    add public router

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -7,14 +7,16 @@ export const LoginScreen = ({ history }) => {
     const { dispatch } = useContext( AuthContext );
 
     const handleLogin = () => {
-        // history.push('/');
+        
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
         dispatch({
             type: types.login,
             payload: {
                 name: 'Alejandro'
             }
         });
-        history.replace('/');
+        history.replace(lastPath);
     }
     return (
         <div className="container mt-5">
